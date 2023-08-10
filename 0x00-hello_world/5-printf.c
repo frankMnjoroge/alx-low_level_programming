47634364a62afff0b64f3442d1a0b3ed136ef8a3
frankMnjoroge <frankmugo@gmail.com>
2023-08-10 16:09:32 -0700

    usin printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include(stdio.h)
+/**
+ * main - A prgram that uses prinf function
+ * Return: 0 (succesful)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+Return (0);
+}
