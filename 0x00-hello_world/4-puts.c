0058809f7a1738e400edf2862e0fa07a448f3e07
frankMnjoroge <frankmugo@gmail.com>
2023-08-10 15:40:00 -0700

    3 files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main -A program demonstrating use of put begining of program excution
+* return :0 (succesful)  
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
