46ba9eca0a16c1d69f5edc512612b46b51b1687a
frankMnjoroge <frankmugo@gmail.com>
2023-08-15 15:50:41 -0700

    a function that checks for alphabetic character.

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include <main.h>
+/**
+ * _islower - function that returns 1 or zero if characters are in low case
+ * @c: parameters
+ *
+ * Return: 0 or 1 (success)
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
