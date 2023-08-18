aacf9404b61940302e04e3303e3be2b29820bd51
frankMnjoroge <frankmugo@gmail.com>
2023-08-18 09:29:19 -0700

    Draw line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,9 +1,18 @@
 #include "main.h"
 /**
- * 
- * *
+ * print_line - draws a straight line
+ * @n: paramter value
+ * Return: 0(success)
  */
 void print_line(int n)
 {
 	int m;
+		if (n <= 0)
+			_putchar('\n');
+		else
+		{
+			for (m = 1 ; m <= n ; m++)
+				_putchar('_');
+			_putchar('\n');
+		}
 }
