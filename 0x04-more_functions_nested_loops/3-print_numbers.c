b3679fedfd9bf91f968c0243ed19cf08027ecc09
frankMnjoroge <frankmugo@gmail.com>
2023-08-18 07:12:41 -0700

    function that prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - Function that that prints 0 to 9
+ * Return: 0 (success)
+ */
+void print_numbers(void)
+{
+	int m;
+
+	for (m = 0 ; m <= 9 ; m++)
+		_putchar(m + '0');
+	_putchar('\n');
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 int mul(int a, int b);
+void print_numbers(void);
 int _isdigit(int c);
 int _isupper(int c);
+int _putchar(char c);
 #endif
