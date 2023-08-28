d67d93a49dc8683fc0dd814d69976033eea1c888
frankMnjoroge <frankmugo@gmail.com>
2023-08-28 13:21:56 -0700

    function that locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* __strchr - locates a character in a string
+* @s: parameter value 1
+* @c: paremeter value 2
+* Return: 0 success
+*/
+char *_strchr(char *s, char c)
+{
+	int m;
+
+	for (m = 0 ; s[m] >= '\0' ; m++)
+	{
+		if (s[m] == c)
+			return (&s[m]);
+	}
+	return (0);
+}
