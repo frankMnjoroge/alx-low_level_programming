8f334da6b7a4ce4d94aa513d0d14f68bcecc7d99
frankMnjoroge <frankmugo@gmail.com>
2023-09-21 06:20:42 -0700

    function that prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,9 +1,25 @@
 #include "lists.h"
 /**
- *
- *
+ * add_node - adds a new node at start of list
+ * @head: pointer to the t_list
+ * @str: string to add to node
+ * Return: address of new list or null if not success
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	
+	list_t *tempo;
+
+	unsigned int length = 0;
+
+	while (str[length])
+		length++;
+	tempo = malloc(sizeof(list_t));
+	if (tempo == NULL)
+		return (NULL);
+	tempo->len = length;
+	tempo->str = strdup(str);
+	tempo->next = (*head);
+	(*head) = tempo;
+
+	return (tempo);
 }
