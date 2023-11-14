b23984851492450a58a4f62b02016989456399cc
frankMnjoroge <frankmugo@gmail.com>
2023-11-14 15:53:54 -0800

    100-operations.so

---
+++ 0x18-dynamic_libraries/combination.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+/**
+ * add - add two numbers
+ * @y: value of paramter one
+ * @z: value of paramter two
+ * return: sum of 2 numbers
+ */
+int add(int y, int z)
+{
+	return (y + z);
+}
+
+/**
+ * sub - difference of two numbers
+ * @y: value one
+ * @z:value two
+ * return: difference
+ */
+int sub(int y, int z)
+{
+	return(y -z);
+}
+/**
+ * mul - multiplication of two numbers
+ * @y: value of num1
+ * @z:value of num 2
+ * return: multiplication
+ */
+int mul(int y, int z)
+{
+	return(y * z);
+}
+/**
+ * div - divides two numbers
+ * @y: value of number one
+ * @z: vaue of  number two
+ * return: division of two numbers
+ */
+int div(int y, int z)
+{
+	return (y / z);
+}
+/**
+ * mod - modulus of two numbers
+ * @y: parameter of value one
+ * @z: parameter of value two
+ * return: reminder of 2 numbers
+ */
+int mod(int y, int z)
+{
+	return (y % z);
+}
+
